8ade3bd01e1297a6f1b0f4f97336f60095ea4268
lucas <lucas.santo@tallos.com.br>
2022-02-18 11:48:42 -0300

    Adicionando produtos

---
+++ src/controllers/produtos.controller.js
@@ -0,0 +1,36 @@
+const knex = require('../db/conexao');
+class ProdutosControllers {
+
+  constructor() {
+    this.conexao = knex;
+  }
+  
+  async create(req, res) {
+    const {
+      nome,
+      bar_code
+    } = req.body;
+    const produtos = await this.conexao('produtos').insert({
+      nome,
+      bar_code
+    }).debug()
+    return res.status(201).json(produtos)
+  }
+  async getAll(req, res) {
+    const produtos = await this.conexao('produtos').debug();
+    return res.status(200).json(produtos);
+  }
+  async getOne(req, res) {
+    const { nome } = req.body;
+    const produtos = await this.conexao('produtos').where('nome', nome).first().debug();
+    return res.status(200).json(produtos);
+  }
+
+  async deleteOne(req, res) {
+    const { nome } = req.body;
+    const produtos = await this.conexao('produtos').del().where('nome', nome).debug();
+    return res.status(200).json(produtos);
+  }
+
+}
+module.exports = ProdutosControllers;
